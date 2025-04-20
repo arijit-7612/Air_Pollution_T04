3210e82d74ff4c863b773133139f85e07a5194fb
DebarghaSamanta <146454432+DebarghaSamanta@users.noreply.github.com>
2025-04-20 08:00:03 +0530

    Corrected code for cassification

--- Classification_Disease_type.R
+++ Classification_Disease_type.R
@@ -55,30 +55,23 @@ y_cardio <- df$CardioRisk
 
 set.seed(42)
 balance_cardio <- SMOTE(X_cardio, y_cardio, dup_size = 4, K = 7)
-X_cardio <- smote_cardio$data[, 1:ncol(X_cardio)]
+X_cardio <- balance_cardio$data[, 1:ncol(X_cardio)]
 y_cardio <- as.factor(balance_cardio$data$class)
 
-train_cardio <- createDataPartition(y_cardio_smote, p = 0.8, list = FALSE)
-X_train_cardio <- X_cardio_smote[train_cardio, ]
-y_train_cardio <- y_cardio_smote[train_cardio]
-X_test_cardio <- X_cardio_smote[-train_cardio, ]
-y_test_cardio <- y_cardio_smote[-train_cardio]
+train_cardio <- createDataPartition(y_cardio, p = 0.8, list = FALSE)
+X_train_cardio <- X_cardio[train_cardio, ]
+y_train_cardio <- y_cardio[train_cardio]
+X_test_cardio <- X_cardio[-train_cardio, ]
+y_test_cardio <- y_cardio[-train_cardio]
 
-# Train model
 rf2 <- randomForest(x = X_train_cardio, y = y_train_cardio, ntree = 100)
 pred_cardio <- predict(rf2, X_test_cardio)
 
 cat("\n : Cardio Risk\n")
 print(confusionMatrix(pred_cardio, y_test_cardio))
 
-
 cat("\nImportance (Respiratory Risk):\n")
 print(importance(rf1))
 
 cat("\nImportance (CArdiovascular Risk):\n")
 print(importance(rf2))
-
-percent_zero <- mean(df$CardioRisk == 0) * 100
-percent_one <- mean(df$CardioRisk == 1) * 100
-cat(sprintf("\n✅ Percentage of CardioRisk = 0: %.2f%%\n", percent_zero))
-cat(sprintf("✅ Percentage of CardioRisk = 1: %.2f%%\n", percent_one))
